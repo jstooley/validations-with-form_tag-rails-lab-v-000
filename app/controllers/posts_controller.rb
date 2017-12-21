fceefcfa7478891af6c9c47bf652671b0c01dc6e
Justin Tooley <jstooley823@gmail.com>
2017-12-21 16:50:26 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,4 +41,8 @@ class PostsController < ApplicationController
   def post_params
     params.permit(:title, :category, :content)
   end
+
+  def set_post!
+    @post = Post.find(params[:id])
+  end
 end
