5a46bf5a1de39dd7eb9f5a15972c9637fd2a4f64
Justin Tooley <jstooley823@gmail.com>
2017-12-21 17:14:57 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get "/posts/:id", to: "posts#show", as: "post"
   get "/posts/new", to: "posts#new", as: "new_post"
-  get "/posts/:id", to: "posts#show", as: "post"
+  post "/psots", to: "posts#create"
   get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
   patch "/posts/:id", to: "posts#update"
 end
