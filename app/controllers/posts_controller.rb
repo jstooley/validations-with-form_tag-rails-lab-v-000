9226646b147c240cd8fb85fe9ac8281b306ce7b2
Justin Tooley <jstooley823@gmail.com>
2017-12-21 17:40:17 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,8 +22,8 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.new(post_params)
-  if @post.valid?
+    @check_post = Post.new(post_params)
+  if @check_post.valid?
     @post = set_post!.update(post_params)
     redirect_to post_path(@post)
   else
