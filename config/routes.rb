ea21b151f0cbe4908ab30f6bdee39abfee07dca5
Justin Tooley <jstooley823@gmail.com>
2017-12-21 17:13:07 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "/authors/:id", to: "authors#show", as: "author"
   post "/authors", to: "authors#create"
   get "/authors/:id/edit", to: "posts#edit", as: "edit_post"
-  patch "/posts/:id", to: "posts#update"
+  patch "/authors/:id", to: "posts#update"
 
   get "/posts/:id", to: "posts#show", as: "post"
   get "/posts/:id/edit", to: "posts#edit", as: "edit_post"
